c4ef64d5dbd81fa6dec8b72d5f747f716a21b844
Gabriel Lobo <gabryellobo@gmail.com>
2025-05-13 00:37:31 +0000

    aventureiro

--- xadrezaventureiro.c
+++ xadrezaventureiro.c
@@ -3,6 +3,7 @@
 int main() {
 
     int B = 0, R = 0;
+    int MVC = 1;
 
     printf("Bem-vindo ao desafio de xadrez\n");
 
@@ -11,11 +12,15 @@ int main() {
         printf("Direita!\n");
     }
 
+    printf("\n");
+
     printf("Bispo se movendo!\n");
     while(B < 5) {
         printf("Cima, Direita!\n");
         B++;
     }
+
+    printf("\n");
 
     printf("Rainha se movendo!\n");
     do {
@@ -23,7 +28,18 @@ int main() {
         R++;
     }while (R < 8);
 
+    printf("\n");
 
+    printf("Movimento do Cavalo\n");
+    for (int C = 0; C < MVC; C++) {
+        int P = 0;
+
+        while (P < 2) {
+            printf("Baixo!!\n");
+            P++;
+        }
+
+        printf("Esquerda!\n");
+    }
     return 0;
 }
-
